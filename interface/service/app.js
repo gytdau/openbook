8729328f7338ca56d57f4d92f4d69bf48dec2598
Gytis Daujotas <gytdau@gmail.com>
2021-09-07 22:12:36 +0100

    Respond with React build to any get request

--- interface/service/app.js
+++ interface/service/app.js
@@ -27,6 +27,8 @@ app.use(function (req, res, next) {
   next(createError(404))
 })
 
+app.get("*", (req, res) => res.sendFile(resolve("build", "index.html")))
+
 // error handler
 app.use(function (err, req, res, next) {
   // set locals, only providing error in development
