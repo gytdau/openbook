238925f0b4d0b6473fdf1ad2037c189d591c21a0
Gytis Daujotas <gytdau@gmail.com>
2021-09-13 23:30:39 +0100

    Reformatting

--- interface/service/app.js
+++ interface/service/app.js
@@ -1,42 +1,41 @@
-var createError = require("http-errors")
-var express = require("express")
-var path = require("path")
-var cookieParser = require("cookie-parser")
-var logger = require("morgan")
-var cors = require("cors")
-require("dotenv").config()
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
+require('dotenv').config();
 
-var booksRouter = require("./routes/books")
+const booksRouter = require('./routes/books');
 
-var app = express()
+const app = express();
 
-app.use(cors())
-app.use(logger("dev"))
-app.use(express.json())
-app.use(express.urlencoded({ extended: false }))
-app.use(cookieParser())
-app.use(express.static(path.join(__dirname, "public")))
+app.use(cors());
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use("/api/books", booksRouter)
+app.use('/api/books', booksRouter);
 
 // serve our React app
-app.use(express.static(path.join(__dirname, "build")))
+app.use(express.static(path.join(__dirname, 'build')));
 
-app.get("*", (req, res) => {
-  console.log("Serve static")
-  res.sendFile(path.join(__dirname, "./build/index.html"))
-})
+app.get('*', (req, res) => {
+  console.log('Serve static');
+  res.sendFile(path.join(__dirname, './build/index.html'));
+});
 
 // error handler
-app.use(function (err, req, res, next) {
+app.use((err, req, res, next) => {
   // set locals, only providing error in development
-  res.locals.message = err.message
+  res.locals.message = err.message;
   // res.locals.error = req.app.get("env") === "development" ? err : {}
-  res.locals.error = err
+  res.locals.error = err;
 
   // render the error page
-  res.status(err.status || 500)
-  res.json(JSON.stringify(res.locals.message))
-})
+  res.status(err.status || 500);
+  res.json(JSON.stringify(res.locals.message));
+});
 
-module.exports = app
+module.exports = app;
--- interface/service/routes/books.js
+++ interface/service/routes/books.js
@@ -1,66 +1,67 @@
-var express = require("express")
-var router = express.Router()
-var { Client, Pool } = require("pg")
+const express = require('express');
 
-var pool = new Pool()
+const router = express.Router();
+const { Client, Pool } = require('pg');
 
-router.get("/get/:title", function (req, res, next) {
-    pool.query(
-        `SELECT * FROM books WHERE slug = $1`,
-        [req.params.title],
-        (err, result) => {
-            const book = result.rows[0]
-            pool.query(
-                `SELECT id, book_id, title, slug FROM chapters WHERE book_id = $1`,
-                [book.id],
-                (err, result) => {
-                    const chapters = result.rows
-                    book.chapters = chapters
-                    res.json(book)
-                }
-            )
-        }
-    )
-})
+const pool = new Pool();
 
-router.get("/image/:fileLocation", function (req, res, next) {
-    pool.query(
-        `SELECT content, format FROM images WHERE location = $1`,
-        [req.params.fileLocation],
-        (err, result) => {
-            const image = result.rows[0]
-            res.contentType(`image/${image.format}`)
-            res.end(image.content)
-        }
-    )
-})
-
-router.get("/chapter/:id", function (req, res, next) {
-    pool.query(
-        `SELECT * FROM chapters WHERE id = $1`,
-        [req.params.id],
-        (err, result) => {
-            const chapter = result.rows[0]
-            res.json(chapter)
-        }
-    )
-})
-
-router.get("/catalog", function (req, res, next) {
-    pool.query("SELECT * FROM books", (err, result) => {
-        const books = result.rows
-        res.json(books)
-    })
-})
-
-router.get("/search/:query", function (req, res, next) {
+router.get('/get/:title', (req, res, next) => {
   pool.query(
-    `SELECT books.id, books.title, books.author, books.slug, books.description, LEFT(chapters.content_stripped, 500) AS sample FROM books LEFT JOIN chapters ON books.id = chapters.book_id WHERE books.title LIKE ('%' || ($1) || '%')  AND chapters.chapter_order = 2`,
+    'SELECT * FROM books WHERE slug = $1',
+    [req.params.title],
+    (err, result) => {
+      const book = result.rows[0];
+      pool.query(
+        'SELECT id, book_id, title, slug FROM chapters WHERE book_id = $1',
+        [book.id],
+        (err, result) => {
+          const chapters = result.rows;
+          book.chapters = chapters;
+          res.json(book);
+        },
+      );
+    },
+  );
+});
+
+router.get('/image/:fileLocation', (req, res, next) => {
+  pool.query(
+    'SELECT content, format FROM images WHERE location = $1',
+    [req.params.fileLocation],
+    (err, result) => {
+      const image = result.rows[0];
+      res.contentType(`image/${image.format}`);
+      res.end(image.content);
+    },
+  );
+});
+
+router.get('/chapter/:id', (req, res, next) => {
+  pool.query(
+    'SELECT * FROM chapters WHERE id = $1',
+    [req.params.id],
+    (err, result) => {
+      const chapter = result.rows[0];
+      res.json(chapter);
+    },
+  );
+});
+
+router.get('/catalog', (req, res, next) => {
+  pool.query('SELECT * FROM books', (err, result) => {
+    const books = result.rows;
+    res.json(books);
+  });
+});
+
+router.get('/search/:query', (req, res, next) => {
+  pool.query(
+    "SELECT books.id, books.title, books.author, books.slug, books.description, LEFT(chapters.content_stripped, 500) AS sample FROM books LEFT JOIN chapters ON books.id = chapters.book_id WHERE books.title LIKE ('%' || ($1) || '%')  AND chapters.chapter_order = 2",
     [req.params.query],
     (err, result) => {
-      const books = result.rows
-      res.json(books)
-    }
-  )
-})
-module.exports = router
+      const books = result.rows;
+      res.json(books);
+    },
+  );
+});
+module.exports = router;
