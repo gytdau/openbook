fbccd203e0facb8b448b0f7a0c1e63c71e856eda
Gytis Daujotas <gytdau@gmail.com>
2022-03-18 02:07:53 +0000

    Add front end tests

---
+++ interface/viewer/src/Home/FooterTest.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import Footer from './Footer';
+
+it('footer renders with credit', async () => {
+    render(<Footer />);
+    await screen.getByText(/Gytis Daujotas/);
+    await screen.getByText(/Sayed Mahmood Alawi/);
+});
